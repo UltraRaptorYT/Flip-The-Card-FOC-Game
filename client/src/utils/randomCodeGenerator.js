b9369e342f0310bf2e26e6207b308693ff054d72
UltraRaptorYT <62703912+UltraRaptorYT@users.noreply.github.com>
2022-03-23 00:58:04 +0800

    remove

--- client/src/utils/randomCodeGenerator.js
+++
@@ -1,9 +0,0 @@
-export default function makeId(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
